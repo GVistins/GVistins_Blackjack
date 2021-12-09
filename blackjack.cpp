fa9cdc7a21e0583aa117fd06007edcc22b37c4ed
02DPGVistins <78017308+GVistins@users.noreply.github.com>
2021-12-09 20:56:44 +0200

    Update blackjack.cpp

--- blackjack.cpp
+++ blackjack.cpp
@@ -2,12 +2,11 @@
 #include <cstdlib>
 #include <string>
 #include <time.h>
+// Defines the integers that the whole code uses in the global scale.
 int choice, card, type, points, epoints;
 int turn = 1;
-int point_system(int card);
-int hit_or_stay();
-int point_system_enemy(int card);
 
+// Contains the entire deck of cards that the code uses by utilizing a multidimensional array.
 std::string deck[4][13] = {{"Ace of Diamonds", "2 of Diamonds", "3 of Diamonds", "4 of Diamonds", "5 of Diamonds", "6 of Diamonds", "7 of Diamonds", "8 of Diamonds", "9 of Diamonds",
                                    "10 of Diamonds", "Jack of Diamonds", "Queen of Diamonds", "King of Diamonds"},
                            {"Ace of Spades", "2 of Spades", "3 of Spades", "4 of Spades", "5 of Spades", "6 of Spades", "7 of Spades", "8 of Spades", "9 of Spades",
@@ -18,7 +17,7 @@ std::string deck[4][13] = {{"Ace of Diamonds", "2 of Diamonds", "3 of Diamonds",
                                    "10 of Clubs", "Jack of Clubs", "Queen of Clubs", "King of Clubs"}};
 
 
-
+// Makes it more readable by making a special function for when you draw an ace instead of putting this cluster of code in the draw_card function.
 int ace(){
     std::cout << "You got an ace, input if you want to receive 1 or 11 points: " << std::endl;
     std::cin >> choice;
@@ -29,6 +28,7 @@ int ace(){
         points += 11;
     }
 }
+// Calculates the enemy points.
 int point_system_enemy(int card){
     if (card == 0){
         if(epoints <= 10) {
@@ -75,6 +75,7 @@ int point_system_enemy(int card){
         epoints += 10;
     }
 }
+// Calculates the points for the player by assigning values in the array to points in the value system of Blackjack.
 int point_system(int card){
     if (card == 0){
         ace();
@@ -116,7 +117,7 @@ int point_system(int card){
         points += 10;
     }
 }
-
+// Draws the card for the player, also giving you the name of the card and showing the total points you have acquired.
 int card_draw() {
     card = rand() % 13;
     type = rand() % 4;
@@ -124,7 +125,7 @@ int card_draw() {
     point_system(card);
     std::cout << "You have: " << points << " points." << std::endl << std::endl;
 }
-
+// Lets the user draw a card or not by asking for their input, and based on that drawing a card or giving the turn to the AI.
 int hit_or_stay(){
     std::cout << "Hit or stay (1 = Hit, 0 = Stay): " << std::endl;
     std::cin >> choice;
@@ -135,7 +136,7 @@ int hit_or_stay(){
         turn = 0;
     }
 }
-
+// Draws a card for the AI.
 int card_draw_enemy(){
     card = rand() % 14;
     type = rand() % 3;
@@ -143,7 +144,7 @@ int card_draw_enemy(){
     point_system_enemy(card);
     std::cout << "The enemy has: " << epoints << " points." << std::endl << std::endl;
 }
-
+// A function that defines the usual turn for the player.
 int player(){
     srand(time(0));
     for(int i=0;i<2;i++){
@@ -157,7 +158,7 @@ int player(){
 
 
 }
-
+// Calculates the enemy turn.
 int enemy(){
     srand(time(0));
     for(int i = 0;i<2;i++){
@@ -173,6 +174,7 @@ int enemy(){
     }while(epoints < 21 & turn == 0);
     turn = 2;
 }
+// Calculate the winning condition, either returning win, lose or draw.
 int winning_condition(){
     for(int i = 0;i<5;i++){
         std::cout << std::endl;
@@ -189,7 +191,7 @@ int winning_condition(){
         std::cout << "Draw!" << std::endl;
     }
 }
-
+// Puts the whole code in a action by calling the player, enemy and winning condition functions.
 int main(){
     player();
     if (turn == 0){
